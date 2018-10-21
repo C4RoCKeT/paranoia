9cd4391e98624e9d07ef5a5ea11aaa1765fbe2a6
Brandon Tilstra <brandontilstra@promocat.nl>
2018-10-21 14:47:00 +0200

    better character view

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -18,50 +18,52 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>Please fill out the following fields to login:</p>
 
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-md-8 col-lg-6">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
             <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
             <?= $form->field($model, 'password')->passwordInput() ?>
 
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-            <div style="color:#999;margin:1em 0">
-                If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+            <div class="row">
+                <div class="col-xs-7">
+                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                </div>
+                <div class="col-xs-5">
+                    <div class="form-group">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-block btn-primary', 'name' => 'login-button']) ?>
+                    </div>
+                </div>
             </div>
-
-            <div class="form-group">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-            </div>
-
             <?php ActiveForm::end(); ?>
+            <div class="social-auth-links text-center">
+                <p>- OR -</p>
+                <?php
+                $authAuthChoice = AuthChoice::begin([
+                    'baseAuthUrl' => ['site/auth'],
+                    'popupMode' => true,
+                ]);
+                foreach ($authAuthChoice->getClients() as $client):
+                    $text = Yii::t('default', 'Sign in using {service}', ['service' => $client->getTitle()]);
+                    switch ($client->getName()) {
+                        case 'google':
+                            $icon = 'fa-google-plus';
+                            $buttonClass = 'btn-google';
+                            break;
+                        default:
+                            $icon = 'fa-sign-in-alt';
+                            $buttonClass = 'btn-' . $client->getName();
+                            break;
+                    }
+                    echo $authAuthChoice->clientLink($client, '<i class="fa ' . $icon . '"></i>' . $text,
+                        ['class' => 'btn btn-block btn-social ' . $buttonClass]);
+                endforeach;
+                AuthChoice::end();
+                ?>
+            </div>
+            <div style="color:#999;margin:1em 0" class="text-center">
+                <?= Html::a('I forgot my password', ['site/request-password-reset']) ?>.
+            </div>
         </div>
-    </div>
-
-    <div class="social-auth-links text-center">
-        <p>- OR -</p>
-        <?php
-        $authAuthChoice = AuthChoice::begin([
-            'baseAuthUrl' => ['site/auth'],
-            'popupMode' => true,
-        ]);
-        foreach ($authAuthChoice->getClients() as $client):
-            $text = Yii::t('default', 'Sign in using {service}', ['service' => $client->getTitle()]);
-            switch ($client->getName()) {
-                case 'google':
-                    $icon = 'fa-google-plus';
-                    $buttonClass = 'btn-google';
-                    break;
-                default:
-                    $icon = 'fa-sign-in-alt';
-                    $buttonClass = 'btn-' . $client->getName();
-                    break;
-            }
-            echo $authAuthChoice->clientLink($client, '<i class="fa ' . $icon . '"></i>' . $text,
-                ['class' => 'btn btn-block btn-social btn-flat ' . $buttonClass]);
-        endforeach;
-        AuthChoice::end();
-        ?>
     </div>
 </div>
